21af8d1a8e1d87273f26f5126d1b11353d21db46
Satyam0004 <satyamkmr04@gmail.com>
2026-01-27 10:49:55 +0530

    UnderStanding Entitieis in JPA

--- src/main/java/com/app/ecom/User.java
+++ src/main/java/com/app/ecom/User.java
@@ -1,9 +1,18 @@
 package com.app.ecom;
 
-import lombok.Data;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.*;
 
 @Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Entity
 public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String firstName;
     private String lastName;
